d3f7600cee5133090d792f9cb74515739c624a58
Neil Alexander <na2482r@gre.ac.uk>
2019-10-08 20:11:37 +0100

    Added scene loading. Removed incorrect tracking.

---
+++ UnityClient/Assets/Scripts/CameraMouseControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouseControl : MonoBehaviour
+{
+
+    private Vector2 mouseDirection;
+
+    private Transform parentBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        parentBody = this.transform.parent.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouseChange = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseDirection += mouseChange;
+
+        this.transform.localRotation = Quaternion.AngleAxis(-mouseDirection.y, Vector3.right);
+
+        parentBody.localRotation = Quaternion.AngleAxis(mouseDirection.x, Vector3.up);
+    }
+}
