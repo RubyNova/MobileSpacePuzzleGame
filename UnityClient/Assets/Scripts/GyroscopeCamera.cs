a37b981f4693b7da0db3f41400f9d988ea4313c9
Neil Alexander <na2482r@gre.ac.uk>
2019-11-14 21:46:27 +0000

    Adding gyroscope camera control

---
+++ UnityClient/Assets/Scripts/GyroscopeCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroscopeCamera : MonoBehaviour
+{
+
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+
+    void Update()
+    {
+        transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, -Input.gyro.rotationRateUnbiased.z);
+    }
+}
