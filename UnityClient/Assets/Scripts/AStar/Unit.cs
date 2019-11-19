1c75953468518b80bb803fd0e1ae8fb41cfca0b0
Matt Jones <matthewj866@gmail.com>
2019-11-19 05:08:12 +0000

    Merged pathfinding branch. Deleted corrupted ProBuilder install.

---
+++ UnityClient/Assets/Scripts/AStar/Unit.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 5;
+    Vector3[] path;
+    int targetIndex;
+
+    private GameObject targetP;
+    void Start()
+    {
+        targetP = GameObject.FindWithTag("Target");
+        target = targetP.transform;
+        PathRequestManager.RequestPath(transform.position,target.position, OnPathFound);
+    }
+
+    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+    {
+        if (pathSuccessful)
+        {
+            path = newPath;
+            targetIndex = 0;
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        Vector3 currentWaypoint = path[0];
+
+        while (true)
+        {
+            if (transform.position == currentWaypoint)
+            {
+                targetIndex++;
+                if (targetIndex >= path.Length)
+                {
+                    // yield is how you exit out a coroutine;
+                    yield break;
+                }
+
+                currentWaypoint = path[targetIndex];
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+    
+    public void OnDrawGizmos() {
+        if (path != null) {
+            for (int i = targetIndex; i < path.Length; i ++) {
+                Gizmos.color = Color.black;
+                Gizmos.DrawCube(path[i], new Vector3(0.3f, 0.3f,0.3f));
+
+                if (i == targetIndex) {
+                    Gizmos.DrawLine(transform.position, path[i]);
+                }
+                else {
+                    Gizmos.DrawLine(path[i-1],path[i]);
+                }
+            }
+        }
+    }
+}
---
+++ UnityClient/Assets/Scripts/TowerDefence/Pad.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using UnityEngine;
+
+public class Pad : MonoBehaviour
+{
+    // Handle placement of turrets and user feedback, as well as if the player
+    // has pressed used the pad / is it free or not?
+    public Color hoverColor;
+    public Vector3 positionOffSet;
+    public Vector3 rotationOffset;
+    
+    //Check if turret is placed or not
+    private GameObject turret;
+    
+    // Reference to renderer component
+    private Renderer rend;
+    private Color startColor;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        //Needs to be implemented onto phone
+        if (turret != null)
+        {
+            Debug.Log("Cannot build there! - TODO: put in canvas.");
+            return;
+        }
+        
+        // Build a turret
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild,transform.position + positionOffSet, transform.rotation);
+    }
+
+    void OnMouseEnter()
+    {
+        //Needs to be implemented onto phone
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        //Needs to be implemented onto phone
+        rend.material.color = startColor;
+    }
+}
