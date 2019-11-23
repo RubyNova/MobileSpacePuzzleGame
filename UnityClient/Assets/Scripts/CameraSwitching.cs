e109c0e0ea7dbac30a0ed8c12cab8cb815eb85f6
Neil Alexander <na2482r@gre.ac.uk>
2019-11-23 22:55:29 +0000

    Adding multiple camera functionality to level

---
+++ UnityClient/Assets/Scripts/CameraSwitching.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitching : MonoBehaviour
+{
+    [SerializeField]
+    private Camera _mainCamera;
+
+    [SerializeField]
+    private Camera _overheadCamera;
+
+    public void ShowOverheadView()
+    {
+        _mainCamera.enabled = false;
+        _overheadCamera.enabled = true;
+    }
+
+    public void ShowFirstPersonView()
+    {
+        _mainCamera.enabled = true;
+        _overheadCamera.enabled = false;
+    }
+}
---
+++ UnityClient/Assets/Scripts/Gyro.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gyro : MonoBehaviour
+{
+
+    private void Start() => Input.gyro.enabled = true;
+
+
+    private void Update() => transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
+
+}
