b24a5709762b45776b1dcddb2a3e6c2d9c173f85
Matt Jones <matthewj866@gmail.com>
2019-12-08 18:33:24 +0000

    Added new CameraEffect controller for PostProcessing shader effects. Fixed naming issue - 'Shader Controllers' is now 'ShaderControllers'.

---
+++ UnityClient/Assets/Scripts/PostProcessing/CameraEffect.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+namespace PostProcessing
+{
+    public class CameraEffect : MonoBehaviour
+    {
+        [SerializeField]
+        private Material _effectMaterial;
+
+        [SerializeField] 
+        private float _intensity = 0.5f;
+
+        [SerializeField]
+        private string _effectName = "CHANGE THIS";
+
+        private int _intensityPropertyId;
+
+        public float Intensity
+        {
+            get => _intensity;
+            set
+            {
+                if (value < 0)
+                {
+                    _intensity = 0;
+                    Debug.LogWarning("An attempt was made to assign a negative intensity. You cannot do this! Value clamped to 0.");
+                    return;
+                }
+                else if (value > 1)
+                {
+                    _intensity = 1;
+                    Debug.LogWarning("An attempt was made to assign a value larger than 1. You cannot do this! Value clamped to 1.");
+                    return;
+                }
+                _intensity = value;
+            }
+        }
+
+        public string EffectName
+        {
+            get => _effectName;
+            set => _effectName = value;
+        }
+
+        private void Start() => _intensityPropertyId = Shader.PropertyToID("_Intensity");
+
+        private void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if (Math.Abs(Intensity) < 0)
+            {
+                Graphics.Blit(source, destination);
+                return;
+            }
+            
+            _effectMaterial.SetFloat(_intensityPropertyId, Intensity);
+            Graphics.Blit(source, destination, _effectMaterial);
+        }
+    }
+}
