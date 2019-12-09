4bebfe31e1e1794cd6583a641f470cccda41db7f
Neil Alexander <na2482r@gre.ac.uk>
2019-12-09 17:56:14 +0000

    Code fixes

--- UnityClient/Assets/Scripts/MobileZoom.cs
+++ UnityClient/Assets/Scripts/MobileZoom.cs
@@ -13,30 +13,22 @@ public class MobileZoom : MonoBehaviour
     private const float YFov = 0.1f;
     private const float ZFov = 100f;
 
-
-    void Update()
+    public void Update()
     {
-
         if (Input.touchCount < 2) return;
-
 
         var touchZero = Input.GetTouch(0);
         var touchOne = Input.GetTouch(1);
 
-
         Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
         Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
-
 
         float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
         float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
 
-
         float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
-
 
         _targetCamera.fieldOfView += deltaMagnitudeDiff * _zoomSpeed;
         _targetCamera.fieldOfView = Mathf.Clamp(_targetCamera.fieldOfView, YFov, ZFov);
-
     }
 }
