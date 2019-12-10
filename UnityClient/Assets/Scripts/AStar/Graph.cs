239e8ad3a698ac01376ef10f548620bb0e4c4b80
JohnJB <neardem1@gmail.com>
2019-12-10 00:02:02 +0000

    Changes to prototype
    This is working needs small changes that I plan to do tomorrow before the lecture. But the UI is a little tricky for me to work with. Doesn't work as it usually does - over to you neil.

--- UnityClient/Assets/Scripts/AStar/Graph.cs
+++ UnityClient/Assets/Scripts/AStar/Graph.cs
@@ -1,4 +1,5 @@
-﻿using System.CodeDom;
+﻿using System;
+using System.CodeDom;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -201,6 +202,12 @@ public class Graph : MonoBehaviour
     {
         float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
         float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+
+/*        if (percentX > 1 || percentX < 0 || percentY > 1 || percentY < 0)
+        {
+            throw new Exception("this is the fucking problem");
+        }*/
+        
         // If the character is outside the grid, this stops invalid coordinates
         percentX = Mathf.Clamp01(percentX);
         percentY = Mathf.Clamp01(percentY);
