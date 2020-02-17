13f5f731b03506fc4ffed1f309f34b8442aa7313
Neil Alexander <na2482r@gre.ac.uk>
2020-02-17 20:42:16 +0000

    Adding functionality to switch cameras when GameObject clicked/touched

---
+++ UnityClient/Assets/Scripts/TouchCameraSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchCameraSwitch : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _cameraChoice;
+
+    [SerializeField]
+    private Transform _allCameras;
+
+    private void OnMouseDown()
+    {
+        foreach (Transform child in _allCameras.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+        _cameraChoice.SetActive(true);
+    }
+
+}
