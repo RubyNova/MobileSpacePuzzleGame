fd0b9bdbbe47b7758d6328c1512776fd4703e542
Neil Alexander <na2482r@gre.ac.uk>
2020-02-18 16:25:20 +0000

    Fixing issue with gyroscope camera still moving when game paused

--- UnityClient/Assets/Scripts/GyroscopeCamera.cs
+++ UnityClient/Assets/Scripts/GyroscopeCamera.cs
@@ -6,5 +6,12 @@ public class GyroscopeCamera : MonoBehaviour
 {
     private void Start() => Input.gyro.enabled = true;
 
-    private void Update() => transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
+    private void Update()
+    {
+        if (Time.timeScale != 0f)
+        {
+            transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, 0);
+        }
+
+    }
 }
