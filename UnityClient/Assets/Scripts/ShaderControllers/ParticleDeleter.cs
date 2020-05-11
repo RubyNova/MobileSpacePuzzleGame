2aceb9a8a8b622161d9281ebcad78a02db83667b
Matt Jones <matthewj866@gmail.com>
2020-05-11 17:38:28 +0100

    Improved turret VFX. Made enemies have a little exploding effect on death.

---
+++ UnityClient/Assets/Scripts/ShaderControllers/ParticleDeleter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace ShaderControllers
+{
+    public class ParticleDeleter : MonoBehaviour
+    {
+        [SerializeField] private float _deleteDelay;
+
+        private float _timeToDelete;
+
+        private void Start() => _timeToDelete = _deleteDelay;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_timeToDelete <= 0)
+            {
+                Destroy(gameObject);
+            }
+
+            _timeToDelete -= Time.deltaTime;
+        }
+    }
+}
